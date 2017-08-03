7be2f30163dd90ca1d3761c4a1660e5102b6d207
Mac User <sabbey37@gmail.com>
2017-08-03 11:45:07 -0400

    Reads input file, converts contents to upper case, writes to an ouput file

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const readline = require('readline');
+const lookup = require('lookup-multicast-dns');
 
 const rl = readline.createInterface({
     input: process.stdin,
@@ -7,16 +8,50 @@ const rl = readline.createInterface({
 });
 
 // Converts content of a file to all caps, prints to console
-rl.question('What is the filename? ', (filename) => {
-    rl.close();   // closes interface
-    fs.readFile(filename, (err, buffer) => {
-        if (err) {
-            console.log(err.message);
-            return;
-        }
-        let content = buffer.toString();
-        let upcased = content.toUpperCase();
-        console.log(upcased);
+// rl.question('What is the filename? ', (filename) => {
+//     rl.close();   // closes interface
+//     fs.readFile(filename, (err, buffer) => {
+//         if (err) {
+//             console.log(err.message);
+//             return;
+//         }
+//         let content = buffer.toString();
+//         let upcased = content.toUpperCase();
+//         console.log(upcased);
+//     });
+// });
+
+// Looks up and prints out IP address for domain name
+// rl.question('URL: ', (domain) => {
+//     rl.close();   // closes interface
+//     lookup(domain, (err, ip) => {
+//         if (err) {
+//             console.log(err.message);
+//             console.log("Site not found.")
+//             return;
+//         }
+//         console.log("IP Address: " + ip);
+//     });
+// });
+
+// Read and write: reads content of input file, converts text to all caps, writes contents to output file
+rl.question('Input file: ', (input) => {
+    rl.question('Output file: ', (output) => {
+        rl.close();   // closes interface
+        fs.readFile(input, (err, buffer) => {
+            if (err) {
+                console.log(err.message);
+                return;
+            }
+            var content = buffer.toString();
+            var upcased = content.toUpperCase();
+            fs.writeFile(output, upcased, (err) => {
+                if (err) {
+                    console.log(err.message);
+                    return;
+                }
+                console.log("Wrote to file " + output);
+            });
+        });
     });
 });
-
