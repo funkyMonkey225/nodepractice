4af73e6bbc5a59288eddae962c1ce8f2702a2cf1
Mac User <sabbey37@gmail.com>
2017-08-07 15:09:56 -0400

    Creates TDD for calculator methods

--- calculator/test/test.js
+++ calculator/test/test.js
@@ -8,6 +8,56 @@ describe('Can do arithmetic', () => {
         it('Can add two positive integers', () => {
             expect(calc.add(1,1)).to.equal(2);
         })
+        it('Can add to zero', () => {
+            expect(calc.add(1,0)).to.equal(1);
+        })
+        it('Cannot add a number and a function', () => {
+            expect(calc.add(1, ()=> {})).to.equal('1()=> {}');
+        })
+    })
+    describe('Can subtract numbers', () => {
+        it('Can subtract two positive integers', () => {
+            expect(calc.subtract(2,1)).to.equal(1);
+        })
+        it('Can subtract two negative integers', () => {
+            expect(calc.subtract(-2, -1)).to.equal(-1);
+        })
+        it('Cannot subtract two arrays', () => {
+            expect(calc.subtract([], [])).to.equal(0);
+        })
+    })
+    describe('Can multiply numbers', () => {
+        it('Can multiply two positive integers', () => {
+            expect(calc.multiply(2,2)).to.equal(4);
+        })
+        it('Can multiply two negative integers', () => {
+            expect(calc.multiply(-2, -5)).to.equal(10);
+        })
+        it('Cannot multiply two functions', () => {
+            expect(calc.multiply(()=> {}, ()=> {})).to.be.NaN;
+        })
+    })
+    describe('Can divide numbers', () => {
+        it('Can divide two positive integers', () => {
+            expect(calc.divide(2,1)).to.equal(2);
+        })
+        it('Can divide two negative integers', () => {
+            expect(calc.divide(-2, -1)).to.equal(2);
+        })
+        it('Cannot divide by 0', () => {
+            expect(calc.divide(2, 0)).to.equal(Infinity);
+        })
+    })
+    describe('Can add numbers asynchronously', () => {
+        it('Can add two integers asynchronously', (done) => {
+            calc.addAsync(3, 2, (val) => {
+                if (val === 5) {
+                    done();
+                } else {
+                    done(new Error('nope'));
+                }
+            })
+        })
     })
 })
 
